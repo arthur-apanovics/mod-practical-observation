ecbccca2b6532b5b406f4768772964c1a7236f67
Jeremy Ruffell <jeremy@ruffell.dev>
2020-07-28 10:48:24 +1200

    Check if User is an Admin.

--- app_manager.php
+++ app_manager.php
@@ -13,12 +13,15 @@ require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
 require_once('lib.php');
 require_once('forms.php');
 
+require_login($course, false, $cm);
+if(!is_siteadmin($USER->id)){
+    throw new coding_exception("User needs to be a Site Administrtor to access this feature.");
+}
 
 $cmid = required_param('cmid', PARAM_INT);
 $learnerid = required_param('learnerid', PARAM_INT);
 
 list($course, $cm) = get_course_and_cm_from_cmid($cmid);
-
 
 $submission = submission::get_submission_for_learner_or_null($cm->instance, $learnerid);
 
